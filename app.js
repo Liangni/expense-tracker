aedf2a6fd4b908aabb5e5e3e61310b972c9dc023
Liangni <light0104@gmail.com>
2021-10-26 17:00:10 +0800

    feat: project init

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+
+const PORT = 3000
+
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('This is my expense-tracker project.')
+})
+
+app.listen(PORT, () => {
+  console.log(`App is running on https://localhost:${PORT}!`)
+})
