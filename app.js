72024726ba30212adca8a0f10b46a88b54fd5e3c
Liangni <light0104@gmail.com>
2021-10-26 21:59:28 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express')
 
+
 const PORT = 3000
 
 const app = express()
+
+require('./config/mongoose')
 
 app.get('/', (req, res) => {
   res.send('This is my expense-tracker project.')
