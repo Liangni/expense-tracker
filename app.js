84a97fe4f285501d177daf1c34159ea46a5688a1
Liangni <light0104@gmail.com>
2021-10-29 11:23:12 +0800

    feat: add index page

--- app.js
+++ app.js
@@ -1,15 +1,52 @@
 const express = require('express')
+const exphbs = require('express-handlebars')
+const category = require('./models/category')
 
+const Record = require('./models/record')
+// const Category = require('./models/category')
 
 const PORT = 3000
-
+const CATEGORY = {
+  家居物業: 'fas fa-home',
+  交通出行: 'fas fa- shuttle',
+  休閒娛樂: 'fas fa-grin-beam',
+  餐飲食品: 'fas fa-utensils',
+  其他: 'fas fa-pen',
+}
 const app = express()
+
 
 require('./config/mongoose')
 
+app.engine('hbs', exphbs({ 
+  defaultLayout: 'main',
+  extname: '.hbs',
+  helpers: {
+    getIcon: function (name, iconSource) { 
+      return iconSource[name]
+    },
+  }
+}))
+app.set('view engine', 'hbs')
+
 app.get('/', (req, res) => {
-  res.send('This is my expense-tracker project.')
+  Record.find()
+    .lean()
+    .then(records => {
+      category.find()
+        .lean()
+        .then( categories => {
+          records.forEach(record => {
+            const recordCategory = categories.find(category => category._id.equals(record.categoryId))
+            record.category = recordCategory.name
+            record.date = record.date.toLocaleDateString()
+          })
+          res.render('index', { records, CATEGORY })
+        })
+    })
+    .catch(err => console.log(err))
 })
+
 
 app.listen(PORT, () => {
   console.log(`App is running on https://localhost:${PORT}!`)
