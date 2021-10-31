733c2ed22bc305555e4265adf46120e7b16be40f
Liangni <light0104@gmail.com>
2021-10-31 20:09:52 +0800

    feat: add message partials

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -3,7 +3,7 @@ const router = express.Router()
 const passport = require('passport')
 const User = require('../../models/user')
 
-router.get('/login',(req, res) => {
+router.get('/login', (req, res) => {
   res.render('login')
 })
 
@@ -17,12 +17,29 @@ router.get('/register', (req, res) => {
 })
 
 router.post('/register', (req, res) => {
-  const {name, email, password, confirmPassword} = req.body
-  User.findOne({email})
+  const { name, email, password, confirmPassword } = req.body
+  const errors = []
+  if (!name || !email || !password || !confirmPassword) {
+    errors.push({ message: '所有欄位都是必填！' })
+  }
+  if (password !== confirmPassword) {
+    errors.push({ message: '密碼與確認密碼不相符！' })
+  }
+  if (errors.length) {
+    return res.render('register', {
+      errors,
+      name,
+      email,
+      password,
+      confirmPassword
+    })
+  }
+  User.findOne({ email })
     .then(user => {
       if (user) {
-        console.log('User already exist.')
+        errors.push({ message: '這個 Email 已經註冊過了。' })
         res.render('register', {
+          errors,
           name,
           email,
           password,
@@ -34,14 +51,15 @@ router.post('/register', (req, res) => {
           email,
           password
         })
-        .then(res.redirect('/'))
-        .catch(err => console.log(err))
+          .then(res.redirect('/'))
+          .catch(err => console.log(err))
       }
     })
 })
 
 router.get('/logout', (req, res) => {
   req.logout()
+  req.flash('success_msg', '你已經成功登出。')
   res.redirect('/users/login')
 })
 
