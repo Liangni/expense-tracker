f240130e3df45afe878c3363219afa88be02fffd
Liangni <light0104@gmail.com>
2021-10-31 21:26:36 +0800

    modify categorySeeder

--- models/seeds/categorySeeder.js
+++ models/seeds/categorySeeder.js
@@ -1,10 +1,13 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
 const db = require('../../config/mongoose')
 const Category = require('../category')
 
 const collection = ['家居物業', '交通出行', '休閒娛樂', '餐飲食品', '其他']
 
 db.once('open', () => {
-
   Promise.all(Array.from(
     { length: 5 }, (_, i) =>
     Category.create({ name: collection[i] })
