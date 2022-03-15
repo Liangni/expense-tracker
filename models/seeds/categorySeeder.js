9e5db058bbc76290a7e4d3e71dbfd8103644adaa
Liangni <light0104@gmail.com>
2022-03-15 18:23:24 +0800

    feat: add iconClass column to categorySchema & modify categorySeeder

--- models/seeds/categorySeeder.js
+++ models/seeds/categorySeeder.js
@@ -5,12 +5,21 @@ if (process.env.NODE_ENV !== 'production') {
 const db = require('../../config/mongoose')
 const Category = require('../category')
 
-const collection = ['家居物業', '交通出行', '休閒娛樂', '餐飲食品', '其他']
+const collection = [
+  { name: '家居物業', iconClass: 'fas fa-home' },
+  { name: '交通出行', iconClass: 'fas fa-shuttle-van' },
+  { name: '休閒娛樂', iconClass: 'fas fa-grin-beam' },
+  { name: '餐飲食品', iconClass: 'fas fa-utensils' },
+  { name: '其他', iconClass: 'fas fa-pen' }
+]
 
 db.once('open', () => {
   Promise.all(Array.from(
     { length: 5 }, (_, i) =>
-      Category.create({ name: collection[i] })
+      Category.create({ 
+        name: collection[i].name,
+        iconClass: collection[i].iconClass
+      })
   ))
     .then(() => {
       console.log('done')
