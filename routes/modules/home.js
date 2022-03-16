b6cc89cbc69c6f11ed7a971d3bc787552b6c3d4d
Liangni <light0104@gmail.com>
2022-03-16 11:09:48 +0800

    refactor: Modify routes & add Mongoose populate() to GET /category/:categoryId

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -27,24 +27,22 @@ router.get('/', (req, res) => {
     .catch(err => console.log(err))
 })
 
-router.get('/filter/:category', (req, res) => {
+router.get('/category/:categoryId', (req, res) => {
   const userId = req.user._id
-  const categoryFilter = req.params.category
+  const categoryId = req.params.categoryId
   let totalAmount = 0
-  Category.findOne({ name: categoryFilter })
-    .then(category => {
-      Record.find({ categoryId: category._id, userId })
-        .lean()
-        .then(records => {
-          totalAmount = 0
-          records.forEach(record => {
-            record.category = categoryFilter
-            record.date = record.date.toLocaleDateString()
-            totalAmount += record.amount
-          })
-          res.render('index', { records, totalAmount })
-        })
+  let recordData
+  Record.find({ categoryId, userId })
+    .populate('categoryId')
+    .lean()
+    .then(records => {
+      recordData = records.map(record => {
+        totalAmount += record.amount
+        return { ...record, date: record.date.toLocaleDateString() }
+      })
+      return Category.find().lean()
     })
+    .then(categories => res.render('index', { records: recordData, totalAmount, categories }))
     .catch(err => console.log(err))
 })
 
